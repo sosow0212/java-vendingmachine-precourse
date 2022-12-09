e44ec8536e4ee19e4560b2840e108cb1d5d8c4e4
sosow0212 <sosow0212@naver.com>
2022-12-09 17:47:41 +0900

    feat : (OutputView) 투입 금액을 물어보는 View 구현

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -13,4 +13,7 @@ public class OutputView {
         System.out.println("상품명과 가격, 수량을 입력해 주세요.");
     }
 
+    public void requestOfInputMoney() {
+        System.out.println("투입 금액을 입력해 주세요.");
+    }
 }
