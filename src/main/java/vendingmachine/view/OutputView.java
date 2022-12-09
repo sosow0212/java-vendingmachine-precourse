83cee49b14b2c456d80838d8411a3b1b92643299
sosow0212 <sosow0212@naver.com>
2022-12-09 17:49:00 +0900

    feat : (OutputView) 구매할 상품을 물어보는 View 구현

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -16,4 +16,12 @@ public class OutputView {
     public void requestOfInputMoney() {
         System.out.println("투입 금액을 입력해 주세요.");
     }
+
+    public void printInputMoney(String money) {
+        System.out.println("투입 금액: " + money);
+    }
+
+    public void requestOfBuyingProduct() {
+        System.out.println("구매할 상품명을 입력해 주세요.");
+    }
 }
