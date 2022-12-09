963bd107c354ea3b33a4ab9d28f33796f17df158
sosow0212 <sosow0212@naver.com>
2022-12-09 17:47:07 +0900

    feat : (OutputView) 상품의 정보를 물어보는 뷰 구현

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -5,4 +5,12 @@ public class OutputView {
         System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
     }
 
+    public void printCoinOfMachineHaving() {
+
+    }
+
+    public void requestOfProductInfo() {
+        System.out.println("상품명과 가격, 수량을 입력해 주세요.");
+    }
+
 }
